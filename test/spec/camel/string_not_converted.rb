fed9bccd40bdbfefd54ff4d1c096f709a88447a0
Scott Bellware <sbellware@gmail.com>
2015-06-12 20:00:32 -0500

    String not converted is tested

--- test/spec/camel/string_not_converted.rb
+++ test/spec/camel/string_not_converted.rb
@@ -1,1 +1,27 @@
-raise 'string_not_converted.rb not implemented'
+require_relative '../spec_init'
+
+describe "String not Converted" do
+  it "Does not convert from under_score to camelCase" do
+    str = 'some_string'
+    converted = Casing::Camel.! str, convert_values: false
+    assert(converted == str)
+  end
+
+  it "Does not convert from PascalCase to camelCase" do
+    str = 'SomeString'
+    converted = Casing::Camel.! str, convert_values: false
+    assert(converted == str)
+  end
+
+  it "Does not convert symbols to strings" do
+    sym = :someString
+    converted = Casing::Camel.! sym, convert_values: false
+    assert(converted == sym)
+  end
+
+  it "Has no effect on camelCase" do
+    str = 'someString'
+    converted = Casing::Camel.! str, convert_values: false
+    assert(converted == str)
+  end
+end
