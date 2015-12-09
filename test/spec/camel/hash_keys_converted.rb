a4c34877222f8524b2445b651ccf8536b4e9ff18
Scott Bellware <sbellware@gmail.com>
2015-12-09 17:33:38 -0600

    Tets log output is controlled at actuation

--- test/spec/camel/hash_keys_converted.rb
+++ test/spec/camel/hash_keys_converted.rb
@@ -5,8 +5,8 @@ describe "Hash Keys Converted to Camel Case" do
     hash = Casing::Controls::Hash.example
     camel_hash = Casing::Camel.(hash)
 
-    __logger.info "\nCamel: Keys\n- - -\n#{camel_hash.pretty_inspect}"
-    __logger.info "\nInput\n- - -\n#{hash.pretty_inspect}"
+    __logger.data "\nCamel: Keys\n- - -\n#{camel_hash.pretty_inspect}"
+    __logger.data "\nInput\n- - -\n#{hash.pretty_inspect}"
 
     Casing::Camel.assure(camel_hash)
   end
--- test/test_init.rb
+++ test/test_init.rb
@@ -1,7 +1,6 @@
 ENV['CONSOLE_DEVICE'] ||= 'stdout'
 ENV['LOG_COLOR'] ||= 'on'
 ENV['LOG_LEVEL'] ||= 'trace'
-ENV['LOG_METADATA'] ||= 'off'
 
 puts RUBY_DESCRIPTION
 
