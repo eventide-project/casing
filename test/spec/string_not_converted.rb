304a6dd27abfd2fa37f816e8b67f38af99ad7606
Scott Bellware <sbellware@gmail.com>
2015-12-13 17:37:49 -0600

    Symbols are preserved

--- test/spec/string_not_converted.rb
+++ test/spec/string_not_converted.rb
@@ -4,21 +4,21 @@ require_relative 'spec_init'
   describe "String not Converted to #{casing.downcase} Case" do
     converter = Casing.const_get(casing)
 
-    it "Does not convert from under_score to #{casing.downcase} case" do
+    it "Does not convert from under score to #{casing.downcase} case" do
       input = Casing::Controls::String::Underscore.example
       converted = converter.(input, values: false)
 
       assert(converted == input)
     end
 
-    it "Does not convert from PascalCase to #{casing.downcase} case" do
+    it "Does not convert from pascal case to #{casing.downcase} case" do
       input = Casing::Controls::String::Pascal.example
       converted = converter.(input, values: false)
 
       assert(converted == input)
     end
 
-    it "Does not convert from CamelCase to #{casing.downcase} case" do
+    it "Does not convert from camel case to #{casing.downcase} case" do
       input = Casing::Controls::String::Camel.example
       converted = converter.(input, values: false)
 
